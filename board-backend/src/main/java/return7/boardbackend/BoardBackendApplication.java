1e39aa427f52247d6692f8ab2ee8aab950056978
Dylan <leehc12dl@naver.com>
2025-12-22 16:43:27 +0900

    backend 뼈대

--- board-backend/src/main/java/return7/boardbackend/BoardBackendApplication.java
+++ board-backend/src/main/java/return7/boardbackend/BoardBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BoardBackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BoardBackendApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BoardBackendApplication.class, args);
+	}
 
 }
