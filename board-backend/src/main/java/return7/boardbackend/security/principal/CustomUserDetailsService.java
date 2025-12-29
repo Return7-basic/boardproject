a0c52af7cc19494bb75c613ed79e8e0955906d88
Dylan <leehc12dl@naver.com>
2025-12-29 10:23:56 +0900

    간단 수정@이희찬

--- board-backend/src/main/java/return7/boardbackend/security/principal/CustomUserDetailsService.java
+++ board-backend/src/main/java/return7/boardbackend/security/principal/CustomUserDetailsService.java
@@ -15,9 +15,9 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
-        User user = userRepository.findByLoginId(id)
-                .orElseThrow(() -> new UserNotFoundException("유저를 찾을 수 없습니다 loginID :: " + id));
+    public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
+        User user = userRepository.findByLoginId(loginId)
+                .orElseThrow(() -> new UserNotFoundException("유저를 찾을 수 없습니다 loginID :: " + loginId));
 
         return new CustomPrincipal(user);
     }
