bb21c7cac31ef41afb5c6d72def5462b1ad0499b
Dylan <leehc12dl@naver.com>
2026-01-05 21:53:43 +0900

    add admin@이희찬

--- board-backend/src/main/java/return7/boardbackend/BoardBackendApplication.java
+++ board-backend/src/main/java/return7/boardbackend/BoardBackendApplication.java
@@ -1,8 +1,17 @@
 package return7.boardbackend;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import return7.boardbackend.entity.User;
+import return7.boardbackend.enums.Authority;
+import return7.boardbackend.repository.UserRepository;
+import return7.boardbackend.service.UserService;
 
+@Slf4j
 @SpringBootApplication
 public class BoardBackendApplication {
 
@@ -10,4 +19,23 @@ public class BoardBackendApplication {
 		SpringApplication.run(BoardBackendApplication.class, args);
 	}
 
+    @Bean
+    public CommandLineRunner commandLineRunner(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return (args) -> {
+            if (!userRepository.existsByLoginId("admin")) {
+                User user = User.builder()
+                        .loginId("admin")
+                        .password(passwordEncoder.encode("admin1234"))
+                        .nickname("admin")
+                        .authority(Authority.ADMIN)
+                        .build();
+
+                userRepository.save(user);
+                log.info("admin has been saved");
+
+                User savedUser = userRepository.findByLoginId("admin").orElseThrow();
+                log.info(savedUser.toString());
+            }
+        };
+    }
 }
